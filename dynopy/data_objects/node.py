cf82976c5de8631c61d9deb0a44ab85e5fac95e7
Jack Center <centerjd88@gmail.com>
2020-08-31 15:55:38 -0600

    updated node object to include cost

--- dynopy/data_objects/node.py
+++ dynopy/data_objects/node.py
@@ -1,14 +1,16 @@
 class Node:
-    def __init__(self, position, time_step, information):
+    def __init__(self, position, cost, information, time_step):
         """
         Data object that holds information for nodes used in motion planning.
         :param position: tuple with x and y locations
-        :param time_step: int for node time_step
+        :param cost: cost of node along path
         :param information: probability the target can be seen from the node position
+        :param time_step: int for node time_step
         """
         self.pos = position
-        self.k = time_step
+        self.c = cost
         self.i = information
+        self.k = time_step
 
     def get_position(self):
         return self.pos
@@ -16,14 +18,20 @@ class Node:
     def set_position(self, pos):
         self.pos = pos
 
-    def get_time_step(self):
-        return self.k
+    def get_cost(self):
+        return self.c
 
-    def set_time_step(self, k):
-        self.k = k
+    def set_cost(self, cost):
+        self.c = cost
 
     def get_information(self):
         return self.i
 
     def set_information(self, i):
         self.i = i
+
+    def get_time_step(self):
+        return self.k
+
+    def set_time_step(self, k):
+        self.k = k
