8e720dd860aea4e64c63b0ee1c2bd535faf64289
Jack Center <centerjd88@gmail.com>
2020-09-12 11:29:44 -0600

    added comments

--- config/config.py
+++ config/config.py
@@ -4,13 +4,13 @@
 
 def get_parameters():
     cfg = {
-        "budget": 20,
-        "step_size": 1,
-        "radius": 1.2,
-        "cycles": 10,
-        "samples": 16,
-        "plot_full": False,
-        "gamma": 0.85
+        "budget": 20,           # distance volunteer can travel
+        "step_size": 1,         # distance traveled each time step
+        "radius": 1.2,          # radius to search for node expansion in
+        "cycles": 10,           # number of times to run expansion
+        "samples": 16,          # number of control inputs to sample
+        "plot_full": False,     # plot troubleshooting information
+        "gamma": 0.85           # chance of detection if target is in cell
     }
 
     return cfg
