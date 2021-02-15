38192b9776d97a47d4528b649ff3882405c6d628
Jack <jack.d.center@gmail.com>
2021-02-14 18:10:56 -0700

    added getters

--- dynopy/data_objects/node_traj.py
+++ dynopy/data_objects/node_traj.py
@@ -3,7 +3,7 @@
 
 
 class Node:
-    def __init__(self, state, u, cost=0, information=0.0, time=0, reward=0.0):
+    def __init__(self, state, u, cost=0, information=0.0, time=0, reward=0.0, fusion={}):
         """
         Data object that holds information for nodes used in motion planning.
         :param state: state object
@@ -19,7 +19,13 @@ class Node:
         self.i = information
         self.k = time
         self.r = reward
-        self.f = []
+        self.f = fusion
+
+    def get_x_position(self):
+        return self.x.get_x_position()
+
+    def get_y_position(self):
+        return self.x.get_y_position()
 
     def get_position(self):
         return self.x.get_position()
@@ -27,5 +33,25 @@ class Node:
     def get_state(self):
         return self.x
 
+    def get_cost(self):
+        return self.c
+
+    def get_information(self):
+        return self.i
+
     def get_time(self):
         return self.k
+
+    def get_fusion(self):
+        return self.f
+
+    def compare_time(self, node):
+        """
+
+        :param node: node to compare time to
+        :return: boolean indicating whether the two times are the same
+        """
+        if self.k == node.get_time():
+            return True
+        else:
+            return False
